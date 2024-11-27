c32ac48e126bd85bcab4e6c4bdb89ee5f3b4920d
Aditi-Wanjari06 <aditiWan2003@gmail.com>
2024-11-27 19:26:06 +0530

    folder structure created

---
+++ portfolio/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './index.css';
+
+import Home from './views/Home/Home'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home />
+  }
+])
+root.render(<RouterProvider router={router}/>)
+ 
