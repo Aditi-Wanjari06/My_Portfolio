33f4c7a8bcd4aa6d054fa3f557f4948b1f0fd2ba
Aditi-Wanjari06 <aditiWan2003@gmail.com>
2025-04-23 12:48:05 +0530

    changes made in educational data

--- portfolio/src/educationData.js
+++ portfolio/src/educationData.js
@@ -1,27 +1,27 @@
 const educationData = [
     {
-        'date': 2016,
+        'date': 2017-2018,
         'title': 'Secondary (Class 10th)',
         'content': 'Maharashtra State Board',
         'marks': '94.00%'
     },
 
     {
-        'date': 2019,
+        'date': 2019-2020,
         'title': 'Senior Secondary (12th Science)',
         'content': 'Maharashtra State Board',
          'marks': '88.15%'
     },
 
     {
-        'date': 2020,
+        'date': 2020-2023,
         'title': 'Bachelors of Science(B.Sc)-PCM',
         'content': 'Shri. Shivaji Science College,Nagpur University (RTMNU)',
          'marks': '80.22%'
     },
 
     {
-        'date': '2023-Present',
+        'date': 2023-Present,
         'title': 'Masters in Computer Application (MCA)',
         'content': 'G.H.Raisoni College Amravati University, Nagpur ',
          'marks': '9.32 CGPA (Year-1)'
